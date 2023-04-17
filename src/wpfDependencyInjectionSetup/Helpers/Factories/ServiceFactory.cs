990d71d8e0c87349aacb73cb8c8821b239fa3a83
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-04-17 15:43:03 +0300

    Implement IServiceFactory

---
+++ src/wpfDependencyInjectionSetup/Helpers/Factories/ServiceFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using wpfDependencyInjectionSetup.Helpers.Abstractions;
+
+namespace wpfDependencyInjectionSetup.Helpers.Factories;
+
+internal class ServiceFactory<T> : IServiceFactory<T>
+{
+    private readonly Func<T> _factory;
+
+    public ServiceFactory(Func<T> factory)
+    {
+        _factory = factory;
+    }
+
+    public T Create()
+    {
+        return _factory();
+    }
+}
