bcb7ea7dc19e4ab75b4e35c0afc29535b2a3c57c
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-04-17 15:43:58 +0300

    Create AddFormFactory extensin method of IServiceCollection

---
+++ src/wpfDependencyInjectionSetup/Helpers/Extensions/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using wpfDependencyInjectionSetup.Helpers.Abstractions;
+using wpfDependencyInjectionSetup.Helpers.Factories;
+
+namespace wpfDependencyInjectionSetup.Helpers.Extensions;
+
+internal static class ServiceExtension
+{
+    internal static void AddFormFactory<TForm>(this IServiceCollection services)
+        where TForm : class
+    {
+        services.AddTransient<TForm>();
+        services.AddSingleton<Func<TForm>>(x => () => x.GetService<TForm>()!);
+        services.AddSingleton<IServiceFactory<TForm>, ServiceFactory<TForm>>();
+    }
+}
