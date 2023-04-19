62eee546a31ee2a6f19c44b566fdfa25308e4746
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-04-19 09:46:21 +0300

    Add CommandBase and AsyncCommandBase

---
+++ src/wpfDependencyInjectionSetup/Helpers/Commands/AsyncCommandBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace wpfDependencyInjectionSetup.Helpers.Commands;
+
+internal abstract class AsyncCommandBase : CommandBase
+{
+	private bool _IsExecuting;
+
+	public bool IsExecuting
+	{
+		get { return _IsExecuting; }
+		set { _IsExecuting = value; OnCanExecuteChanged(); }
+	}
+
+    public override bool CanExecute(object? parameter)
+    {
+        return IsExecuting &&  base.CanExecute(parameter);
+    }
+    public override async void Execute(object? parameter)
+    {
+        IsExecuting = true;
+        await ExecuteAsync(parameter);
+        IsExecuting = false;
+    }
+
+    public abstract Task ExecuteAsync(object? parameter);
+}
---
+++ src/wpfDependencyInjectionSetup/Helpers/Commands/CommandBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfDependencyInjectionSetup.Helpers.Commands;
+
+internal abstract class CommandBase : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    // Derived classe con override the CanExecute method
+    public virtual bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+    //Derived classe has to implement the method
+    public abstract void Execute(object? parameter);
+
+    protected void OnCanExecuteChanged() 
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
