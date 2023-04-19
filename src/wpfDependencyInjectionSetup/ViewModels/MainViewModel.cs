cd61e504010f66b726bb6116d01b0caf78eadc44
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-04-19 09:42:22 +0300

    Add MainViewModel

---
+++ src/wpfDependencyInjectionSetup/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using wpfDependencyInjectionSetup.Helpers.Stores;
+
+namespace wpfDependencyInjectionSetup.ViewModels;
+
+internal class MainViewModel : ViewModelBase
+{
+    private readonly NavigationStore _navigationStore;
+    public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
+
+    public MainViewModel(NavigationStore navigationStore)
+    {
+        _navigationStore = navigationStore;
+        _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChamged;
+    }
+
+    private void OnCurrentViewModelChamged()
+    {
+        OnPropertyChanged(nameof(CurrentViewModel));
+    }
+}
